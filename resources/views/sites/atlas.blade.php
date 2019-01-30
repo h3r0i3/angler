93da10578b7017dcfb5f079da20b2e359e7da149
Łukasz Giermach <h3r0i3@gmail.com>
2019-01-30 21:37:08 +0100

    dodano wstepny zaraz wygladu interfejsu dla altasu ryb i okresu ochronnego

--- resources/views/sites/atlas.blade.php
+++ resources/views/sites/atlas.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.app')
 
 @section('body')
-W tym miejscu będzie znajdował się atlas ryb wraz ze zdjęciami, opisem itp.
-<br><br>
+<h4>Aby wyświetli informacje związane z interesującą rybą, 
+wybierz nazwę gatunku z rozwijanej listy znajdującej się poniżej:</h4>
+<br>
 <form action="...">
-	<select name="nazwa">
-		<option>Tu wpisz pierwszą możliwość</option>
-		<option>Tu wpisz drugą możliwość</option>
-		(...)
-	</select>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+		  <label class="input-group-text" for="inputGroupSelect01">Wybierz nazwę ryby: </label>
+		</div>
+		<select class="custom-select" id="inputGroupSelect01">
+		  <option selected>Wybierz...</option>
+		  <option value="1">Rybka z BD 1</option>
+		  <option value="2">Rybka z BD 2</option>
+		  <option value="3">Rybka z BD 3</option>
+		</select>
+	  </div>
+
+	<div class="form-group">
+        <button class="btn btn-primary"> Szukaj </button>
+    </div>
 </form>
 @endsection
--- resources/views/sites/ochrona.blade.php
+++ resources/views/sites/ochrona.blade.php
@@ -4,13 +4,21 @@
 Po wybraniu danego gatunku ryby program będzie informował w oparciu o bieżącą datę, czy złowiona zdobycz 
 znajduje się w chwili obecnej pod ochroną, lub też po wpisaniu rozmiaru danego gatunku, czy ma za mały rozmiar, by została złowiona.  
 <br><br><br>
-<h3>Okresy ochronne ryb, to czas, w którym łowienie danych okazów jest zabronione. Okresy ochronne ryb zbiegają się w czasie przede wszystkim z momentem największego rozrodu danych gatunków, kiedy powinny być poddane szczególnej ochronie. <h3> 
+<h4>Okresy ochronne ryb, to czas, w którym łowienie danych okazów jest zabronione. Okresy ochronne ryb zbiegają się w czasie przede wszystkim z momentem największego rozrodu danych gatunków, kiedy powinny być poddane szczególnej ochronie. </h4> 
 <br>
 <form action="{{route('sites.zapisz')}}" method="post"> <!-- formularz wysyłany za pomocą metody POST -->
     
-    <div class="form-group">
-        <input type="text" name="title" class="form-control" placeholder="Podaj nazwę ryby">
-    </div>
+    <div class="input-group mb-3">
+		<div class="input-group-prepend">
+		  <label class="input-group-text" for="inputGroupSelect01">Wybierz nazwę ryby: </label>
+		</div>
+		<select class="custom-select" id="inputGroupSelect01">
+		  <option selected>Wybierz...</option>
+		  <option value="1">Rybka ochrona z BD 1</option>
+		  <option value="2">Rybka ochrona z BD 2</option>
+		  <option value="3">Rybka ochrona z BD 3</option>
+		</select>
+	  </div>
 
     <div class="form-group">
         <button class="btn btn-primary"> Szukaj </button>
